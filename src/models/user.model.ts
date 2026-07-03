4706bdfd479f04840597489fc20e45a18b9bf6c2
Rubina <rubinasubedi18@gmail.com>
2026-07-03 15:49:43 +0545

    set up user schema model

---
+++ src/models/user.model.ts
@@ -0,0 +1,36 @@
+
+import mongoose from "mongoose"
+//* user schema
+const userSchema = new mongoose.Schema({
+    full_name:{
+        type: String,
+        required: [true, "full_name is required"],
+        minLength : [3, "name must be atleast 3 character long"],
+        triim: true,
+    },
+    email:{
+        type:String,
+        required: [true, "email is required"],
+        unique: [true, "user already exists with provided with email"],
+        trim: true,
+    },
+    password:{
+        type:String,
+        required: [true,"password is required"],
+
+    },
+ profile_image: {
+        type:String,
+
+    },
+phone:{
+         type:String,
+         required: false,
+         maxLength: [10,"phone number atmost be 10 digits long"],
+
+    }
+}, {timestamps: true});
+
+//* user model 
+const User = mongoose.model("user", userSchema);
+export  default User;
