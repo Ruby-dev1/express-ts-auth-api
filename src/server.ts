903c9fcabdbfbd6b45e3e26eb811173480304f4d
Rubina <rubinasubedi18@gmail.com>
2026-07-19 16:10:23 +0545

    graceful shutdown and process error handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 // import dotenv from "dotenv";
 // dotenv.config();
+
+process.on("uncaughtException",(error)=>{
+    console.log("uncaughtEception",error);
+    process.exit(1);
+});
+
 import "dotenv/config";
 import app from "./app";
 import { connectDatabase } from "./config/db.config";
 import ENV_CONFIG from "./config/env.config";
 import { verifyMailServerConnection } from "./config/nodemailer.config";
+import mongoose from "mongoose";
 
 const PORT = ENV_CONFIG.PORT ;
  const DB_URI = ENV_CONFIG.DB_URI!;
@@ -20,10 +27,23 @@ connectDatabase(DB_URI);
 
 //* listens 
 
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`server is running at http://localhost:${PORT}`);
 
     verifyMailServerConnection();
     //sendEmail();
+});
+
+process.on("unhandledRejection",(error)=>{
+    console.log("unhandledRejection",error);
+    process.exit(1);
+});
+
+process.on("SIGINT",()=>{
+    console.log("SIGINT");
+    server.close(async(error)=>{
+        await mongoose.disconnect();
+        process.exit(0);
+    })
 });
 
